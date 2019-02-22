227588549a2dcdeb00d50c4af9253c3dc89f71de
DianaSei <di.hom26@mail.ru>
2019-02-22 12:21:45 +0800

    creating model vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,6 @@
-class Vote < ActiveRecord::Base
-	validates :recipe_id, uniqueness: {scope: :user_id}
-	
-	belongs_to :recipe
-	belongs_to :user
-
+class Vote < ApplicationRecord
+  validates :recipe_id, uniqueness: {scope: :user_id}
+  
+  belongs_to :recipe
+  belongs_to :user
 end
